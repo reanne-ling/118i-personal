5758f4e017e6d942f33dcb74e6f471252c4bb6ee
reanne-ling <reanneling6@gmail.com>
2025-04-07 18:34:42 -0700

    Update page_1.py

---
+++ pages/page_1.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import openai
+
+st.markdown("# Eligibility & Requirements")
+st.sidebar.markdown("# Eligibility & Requirements")
+
+st.subheader('Qualifications for EIH', divider= 'color')
+
+st.subheader('Required Documents', divider= 'color')
+
+st.subheader('FAQs for common situations', divider= 'color')
+
+st.subheader('References', divider= 'color')
